43d6e885b03b92476637dd85c6ba3718ddeb5b56
kwackjiyong <rhkrwldyd@gmail.com>
2021-01-06 14:29:32 +0900

    톰캣 300번 체크

--- tomcat9/bin/driver_ch.sh
+++ tomcat9/bin/driver_ch.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 tomcatCnt=`ps -ef | grep AllLookHoneyLook | grep -v grep | grep -v vi | wc -l`
-
-while :
+cnt = 0
+while [cnt -lt 300]
 do 
-    if [ $tocmatCnt -gt 0 ]; then
+    cnt=$(($cnt+1))
+    if [ $tocmatCnt -gt 0 ]
+    then
         chmod 755 /var/lib/jenkins/workspace/AllLookHoneyLook/tomcat9/webapps/AllLookHoneyLook/resources/chromedriver/chromedriver
         break
     fi

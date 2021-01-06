9d240a34e7c64346a9fd136e1917be8010eb8b26
kwackjiyong <rhkrwldyd@gmail.com>
2021-01-06 14:02:03 +0900

    드라이버 체크 쉘스크립트

---
+++ tomcat9/bin/driver_ch.sh
@@ -0,0 +1,8 @@
+tomcatCnt=`ps -ef | grep AllLookHoneyLook | grep -v grep | grep -v vi | wc -l`
+
+while [ $tomcatCnt -gt 0]
+do 
+    if [ $tocmatCnt -gt 0 ] then
+        chmod 755 /var/lib/jenkins/workspace/AllLookHoneyLook/tomcat9/webapps/AllLookHoneyLook/resources/chromedriver/chromedriver
+        break
+done
